68935539199d6473e42a939808b4d40e24f2bc7b
mfrancois <mfrancois@big-boss-studio.com>
2018-04-29 20:45:12 +0200

    Build the app

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="app">
+        <div class="center-holder">
+            <Player/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Player from './components/Player.vue'
+
+    export default {
+        name: 'app',
+        components: {
+            Player
+        }
+    }
+</script>
+
+<style>
+    body{
+        background-color: black;
+        width: 100%;
+        height: 100%;
+    }
+
+    .center-holder{
+        text-align: center;
+        margin: auto;
+        max-width: 500px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translateX(-50%) translateY(-50%);
+    }
+
+    @media screen and (max-width: 1024px)
+    {
+        .center-holder{
+            top: 30px;
+            transform: translateX(-50%) translateY(0%);
+        }
+    }
+</style>
---
+++ src/components/Player.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="player-holder">
+    <iframe src="https://www.radioking.com/widgets/player/player.php?id=145142&c=%23ffffff&c2=%232c3e50&ii=&p=1&pp=1&i=1&eq=1&po=1&t=1&f=v&v=2&s=0&li=0&h=365&l=470&a=0&plc=0" name="idFrame" id="idFrame" style="border-radius: 5px; width:275px;height:365px; min-height: 365px;" frameBorder="0"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Player'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .player-holder{
+    width: 100%;
+    height: 100%;
+  }
+</style>
